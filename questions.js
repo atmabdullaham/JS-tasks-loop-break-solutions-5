fcabacb699a720d205ed962e02d2c03070eed2e2
atmabdullaham <atmabdullaham@gmail.com>
2024-11-12 14:15:25 +0600

    first commit

---
+++ questions.js
@@ -0,0 +1,9 @@
+// 1. You have an array of grades. Write a for loop to find the first failing grade (less than 50). Use break to stop the loop once you find it.
+
+// 2. You are checking items in stock. If you find any item marked "out of stock", stop checking further. Use break to exit the loop when found.
+
+// 3. Given an array of file sizes (in MB), find the first file larger than 500 MB. Use break to stop the loop once you find it.
+
+// 4. You have a list of event attendees. If you find a VIP guest, stop checking further. Use break when you find "VIP".
+
+// 5. Write a for loop to find the first item in an array of prices that is less than $20. Use break to stop the loop when found.
