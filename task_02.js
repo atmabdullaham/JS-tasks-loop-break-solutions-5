ad5e20551b44ab796c7452270bb11c966b30746d
atmabdullaham <atmabdullaham@gmail.com>
2024-11-17 22:29:23 +0600

    added the solutions for task_02

--- task_02.js
+++ task_02.js
@@ -1,1 +1,12 @@
-// 2. You are checking items in stock. If you find any item marked "out of stock", stop checking further. Use break to exit the loop when found.
+// // 2. You are checking items in stock. If you find any item marked "out of stock", stop checking further. Use break to exit the loop when found.
+
+// 5. Finding an Available Parking Spot
+// Problem:
+// You are managing a parking lot.You have a list of spots, some of which are marked as "occupied." Your task is to find the first available spot.If a spot is available, stop and assign it.
+
+let parkingSpots = ["Occupied", "Occupied", "Available", "Occupied", "Available"];
+for (let i = 0; i < parkingSpots.length; i++) {
+ if (parkingSpots[i] === 'Available') {
+  break;
+ } console.log(parkingSpots[i]);
+}
